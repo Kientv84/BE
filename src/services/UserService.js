627ba89f821eefd30d4de762268279a15ca612bf
Admin <dinhtrieu.15062002@gmail.com>
2023-12-06 10:05:06 +0700

    update

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,6 @@
 const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const { generalRefreshToken, generalAccessToken } = require('./JwtService')
-const mailer = require('../Utils/mailer')
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
