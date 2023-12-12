e57e43262181d46845c68c55a6e667852f9d578f
Hưng Võ <80050392+huwng12@users.noreply.github.com>
2023-12-12 11:55:05 +0700

    update payment for admin

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -9,5 +9,8 @@ router.get('/get-details-order/:id', OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id', authUserMiddleWare, OrderController.cancelOrderDetails)
 router.get('/get-all-order', authMiddleWare, OrderController.getAllOrder)
 router.put('/update-delivery-state/:id', authMiddleWare, OrderController.updateDeliveryState)
+router.put('/update-payment-state/:id', authMiddleWare, OrderController.updatePaymentState)
+router.delete('/delete/:id', authMiddleWare, OrderController.deleteOrder)
+router.post('/delete-many', authMiddleWare, OrderController.deleteMany)
 
 module.exports = router
