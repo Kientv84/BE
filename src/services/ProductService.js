6bd7c8d18a9fba69c52871bd63e4953cf447cd4b
Hưng Võ <80050392+huwng12@users.noreply.github.com>
2023-12-23 21:08:35 +0700

    update

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -173,47 +173,96 @@ const deleteProduct = (id) => {
 //     })
 // }
 
-const getAllProduct = async (limit, page, sort, filter) => {
-    try {
-        const query = Product.find();
+// const getAllProduct = async (limit, page, sort, filter) => {
+//     try {
+//         const query = Product.find();
 
-        // Apply filtering
-        if (filter) {
-            const [label, value] = filter;
-            query.find({ [label]: { $regex: value } });
+//         // Apply filtering
+//         if (filter) {
+//             const [label, value] = filter;
+//             query.find({ [label]: { $regex: value } });
+//         }
+
+//         // Apply sorting
+//         if (sort) {
+//             const [sortOrder, sortField] = sort;
+//             const sortObject = { [sortField]: sortOrder };
+//             query.sort(sortObject);
+//         }
+
+//         // Apply pagination
+//         if (limit && page) {
+//             const skipAmount = limit * (page - 1);
+//             query.skip(skipAmount).limit(limit);
+//         }
+
+//         const [allProduct, totalProduct] = await Promise.all([
+//             query.lean().select('_id name image price rating discount type selled').exec(),
+//             Product.countDocuments().exec()
+//         ]);
+
+//         return {
+//             status: 'OK',
+//             message: 'Get all product SUCCESS',
+//             data: allProduct,
+//             totalProduct,
+//             currentPage: page + 1,
+//             totalPage: Math.ceil(totalProduct / limit),
+//         };
+//     } catch (error) {
+//         console.error('Error generating tokens:', error);
+//         throw error;
+//     }
+// };
+
+const getAllProduct = (limit, page, sort, filter) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const totalProduct = await Product.count()
+            let allProduct = []
+            if (filter) {
+                const label = filter[0];
+                const allObjectFilter = await Product.find({ [label]: { '$regex': filter[1] } }).limit(limit).skip(page * limit).sort({ createdAt: -1, updatedAt: -1 })
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    data: allObjectFilter,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                })
+            }
+            if (sort) {
+                const objectSort = {}
+                objectSort[sort[1]] = sort[0]
+                const allProductSort = await Product.find().limit(limit).skip(page * limit).sort(objectSort).sort({ createdAt: -1, updatedAt: -1 })
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    data: allProductSort,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                })
+            }
+            if (!limit) {
+                allProduct = await Product.find().sort({ createdAt: -1, updatedAt: -1 })
+            } else {
+                allProduct = await Product.find().limit(limit).skip(page * limit).sort({ createdAt: -1, updatedAt: -1 })
+            }
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: allProduct,
+                total: totalProduct,
+                pageCurrent: Number(page + 1),
+                totalPage: Math.ceil(totalProduct / limit)
+            })
+        } catch (e) {
+            reject(e)
         }
-
-        // Apply sorting
-        if (sort) {
-            const [sortOrder, sortField] = sort;
-            const sortObject = { [sortField]: sortOrder };
-            query.sort(sortObject);
-        }
-
-        // Apply pagination
-        if (limit && page) {
-            const skipAmount = limit * (page - 1);
-            query.skip(skipAmount).limit(limit);
-        }
-
-        const [allProduct, totalProduct] = await Promise.all([
-            query.lean().select('_id name image price rating discount type selled').exec(),
-            Product.countDocuments().exec()
-        ]);
-
-        return {
-            status: 'OK',
-            message: 'Get all product SUCCESS',
-            data: allProduct,
-            totalProduct,
-            currentPage: page + 1,
-            totalPage: Math.ceil(totalProduct / limit),
-        };
-    } catch (error) {
-        console.error('Error generating tokens:', error);
-        throw error;
-    }
-};
+    })
+}
 
 const deleteManyProduct = (ids) => {
     return new Promise(async (resolve, reject) => {
