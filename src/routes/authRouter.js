96fc09e38bccf63aa2a124e589fd9b697c08b7cc
Admin <dinhtrieu.15062002@gmail.com>
2024-09-23 04:40:17 +0700

    login with facebook

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -24,6 +24,27 @@ router.get(
   }
 );
 
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", {
+    scope: ["email"],
+    session: false,
+  })
+);
+
+router.get(
+  "/facebook/callback",
+  (req, res, next) => {
+    passport.authenticate("facebook", (err, profile) => {
+      req.user = profile;
+      next();
+    })(req, res, next);
+  },
+  (req, res) => {
+    res.redirect(`${process.env.URL_CLIENT}login-success/${req.user?.id}`);
+  }
+);
+
 router.post("/login-success", authController.loginSuccess);
 
 module.exports = router;
